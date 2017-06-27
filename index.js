5e45da1f34a51e7af793efe9e257bcf73863f004
toyjack <toyjack@gmail.com>
2017-06-28 01:22:31 +0900

    added coinbase price

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-let path=require('path')
-const envPath=path.normalize(__dirname + '/.env')
+let path = require('path')
+const envPath = path.normalize(__dirname + '/.env')
 require('date-utils')
 let axios = require('axios')
 let dotenv = require('dotenv')
-dotenv.config({path: envPath})
+dotenv.config({
+    path: envPath
+})
 dotenv.load()
 let sg = require('sendgrid')(process.env.SENDGRID_API_KEY)
 let helper = require('sendgrid').mail;
-
 
 const from = process.env.SEND_FROM
 const tos = process.env.SEND_TO.split(',')
 const huobiAPI = 'http://api.huobi.com/staticmarket/ticker_btc_json.js'
 const bitflyerAPI = 'https://bitflyer.jp/api/echo/price'
+const coinbaseAPI = 'https://api.coinbase.com/v2/prices/BTC-CNY/buy'
 
-function sendMail(to,contentBody) {
+function sendMail(to, contentBody) {
     let fromEmail = new helper.Email(from);
     let toEmail = new helper.Email(to);
     let subject = '差价';
@@ -36,6 +38,7 @@ function sendMail(to,contentBody) {
     });
 }
 
+//api grpup for axio
 function getHuobi() {
     return axios.get(huobiAPI)
 }
@@ -44,24 +47,33 @@ function getBitflyer() {
     return axios.get(bitflyerAPI)
 }
 
+function getCoinbase() {
+    return axios.get(coinbaseAPI)
+}
+
 function getRate() {
     return axios.get('http://api.k780.com/?app=finance.rate_cnyquot&curno=JPY&&appkey=10003&sign=b59bc3ef6191eb9f747dd4e83c99f2a4&format=json')
 }
 
-axios.all([getHuobi(), getBitflyer(), getRate()])
-    .then(axios.spread(function (huobi, bitflyer, rate) {
+
+axios.all([getHuobi(), getBitflyer(), getRate(), getCoinbase()])
+    .then(axios.spread(function (huobi, bitflyer, rate, basecoin) {
         let curHuobi = huobi.data.ticker.sell
         let curRate = parseFloat(rate.data.result.JPY.BOC.se_sell)
         let curBitflyer = parseInt(bitflyer.data.ask / 100 * curRate)
+        let curCoinbase = parseInt(basecoin.data.data.amount)
         let curPriceGap = curHuobi - curBitflyer
         let dt = new Date()
         let curTime = dt.toFormat('YYYY/MM/DD HH24:MI:SS')
+        
+
         let message = curTime + ' 火币：' + curHuobi + '元，Bitflyer：' + curBitflyer + '元，汇率（中行现汇卖出）：' + curRate + '，差价（中-日）：' + curPriceGap + '。'
+        message += 'Coinbase价格：' + curCoinbase+'元。'
         console.log(message)
         tos.forEach(function(to){
-            sendMail(to,message)
+        sendMail(to,message)
         })
     }))
-    .catch(function(error){
-        sendMail(to,error)
+    .catch(function (error) {
+        sendMail(to, error)
     })
